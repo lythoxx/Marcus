0f6158e526cb612805a4fe4119ef238da7e8392a
lythox <max.broudre@gmx.de>
2024-02-11 17:44:37 +0100

    propeer descriptions

--- src/utils.py
+++ src/utils.py
@@ -140,3 +140,124 @@ def translate_en(text):
     response = request.json()
 
     return response[0]['translations'][0]['text']
+
+def get_weather_descriptions(code: int, main: str) -> str | None:
+    match main:
+        case "Thunderstorm":
+            match code:
+                case 200:
+                    return "Gewitter mit leichtem Regen"
+                case 201:
+                    return "Gewitter mit Regen"
+                case 202:
+                    return "Gewitter mit starkem Regen"
+                case 210:
+                    return "Leichtem Gewitter"
+                case 211:
+                    return "Gewitter"
+                case 212:
+                    return "Starkem Gewitter"
+                case 221:
+                    return "Schwerem Gewitter"
+                case 230:
+                    return "Gewitter mit leichtem Niesel"
+                case 231:
+                    return "Gewitter mit Niesel"
+                case 232:
+                    return "Gewitter mit starkem Niesel"
+                case _:
+                    return None
+        case "Drizzle":
+            match code:
+                case 300:
+                    return "Leichtem Niesel"
+                case 301:
+                    return "Niesel"
+                case 302:
+                    return "Starkem Niesel"
+                case 310:
+                    return "Leichtem Nieselregen"
+                case 311:
+                    return "Nieselregen"
+                case 312:
+                    return "Starkem Nieselregen"
+                case 313 | 321:
+                    return "Sprühregen"
+                case 314:
+                    return "Starkem Sprühregen"
+                case _:
+                    return None
+        case "Rain":
+            match code:
+                case 500:
+                    return "Leichtem Regen"
+                case 501:
+                    return "Regen"
+                case 502:
+                    return "Starkem Regen"
+                case 503:
+                    return "Sehr starkem Regen"
+                case 504:
+                    return "Schwerem Regen"
+                case 511:
+                    return "Gefrierendem Regen"
+                case 520:
+                    return "Leichtem Sprühregen"
+                case 521:
+                    return "Sprühregen"
+                case 522 | 531:
+                    return "Starkem Sprühregen"
+                case _:
+                    return None
+        case "Snow":
+            match code:
+                case 600 | 620:
+                    return "Leichtem Schneefall"
+                case 601 | 621:
+                    return "Schneefall"
+                case 602 | 622:
+                    return "Starkem Schneefall"
+                case 611 | 616:
+                    return "Schneeregen"
+                case 612 | 615:
+                    return "Leichtem Schneeregen"
+                case 613:
+                    return "Starkem Schneeregen"
+                case _:
+                    return None
+        case "Clear":
+            return "Klarem Himmel"
+        case "Clouds":
+            match code:
+                case 801:
+                    return "Wenigen Wolken"
+                case 802:
+                    return "Leicht Bewölktem Himmel"
+                case 803:
+                    return "Bewölktem Himmel"
+                case 804:
+                    return "Stark bewölktem Himmel"
+                case _:
+                    return None
+        case _: # 7xx codes
+            match code:
+                case 701:
+                    return "Nebel"
+                case 711:
+                    return "Rauch"
+                case 721:
+                    return "Dunst"
+                case 731 | 761:
+                    return "Verstaubter Luft"
+                case 741:
+                    return "Starkem Nebel"
+                case 751:
+                    return "Sandstürmen"
+                case 762:
+                    return "Vulkanasche in der Luft"
+                case 771:
+                    return "Sturm"
+                case 781:
+                    return "Tornados"
+                case _:
+                    return None
