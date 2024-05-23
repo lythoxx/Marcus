9a864f7ed8ed09a65d10d76c6534c49dfdbc7585
lythox <max.broudre@gmx.de>
2024-05-24 00:25:18 +0200

    implement history (not tested)

--- src/components/gpt.py
+++ src/components/gpt.py
@@ -8,14 +8,16 @@ class GPT:
     def __init__(self):
         self.config = Config.get_config("config")
         self.model = OpenAI(api_key=Config.get_config("config")["openai_key"])
+        self.history = list()
+        self.history.append({"role": "system", "content": f"Your name is {Config.get_name()}. You are a voice activated AI assistant. You are supposed to respond naturally, so do not read out the link to a source, but tell me the name of the source only. Do not read or create code. Instead explain the concepts behind the code. Do not suggest to give links to websites. Your answers will be read out, so do not add links. Answer only in German. When asked your list of features currently consists of: ['Playing music', 'Weather forecast', 'Setting alarms']. You do not execute these features, but instead tell the user that they have to excute the corresponding command to execute these features."})
+
 
     def prompt(self, prompt):
+        self.history.append({"role": "user", "content": prompt})
         completion = self.model.chat.completions.create(
             model=self.config["gpt_model"],
-            messages=[
-                {"role": "system", "content": f"Your name is {Config.get_name()}. You are a voice activated AI assistant. You are supposed to respond naturally, so do not read out the link to a source, but tell me the name of the source only. Do not read or create code. Instead explain the concepts behind the code. Do not suggest to give links to websites. Your answers will be read out, so do not add links. Answer only in German. When asked your list of features currently consists of: ['Playing music', 'Weather forecast', 'Setting alarms']. You do not execute these features, but instead tell the user that they have to excute the corresponding command to execute these features."},
-                {"role": "user", "content": prompt}
-            ],
+            messages=self.history,
             max_tokens=500
         )
+        self.history.append({"role": "assistant", "content": completion.choices[0].message.content})
         return completion.choices[0].message.content
